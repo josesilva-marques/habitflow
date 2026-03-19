3fb4bbb84e23fe73e84d8c33d703d9eee08e3260
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-03-19 16:37:07 -0300

    dia5

--- main.py
+++ main.py
@@ -28,3 +28,12 @@ def criar_habito(habito: Habito):
     contador_id += 1
     habitos_db.append(habito)
     return habito
+
+from fastapi import FastAPI, HTTPException
+@app.delete("/habitos/{habito_id}")
+def deletar_habito(habito_id: int):
+    for i, h in enumerate(habitos_db):
+        if h.id == habito_id:
+            habitos_db.pop(i)
+            return {"mensagem": "Habito deletado com sucesso"}
+    raise HTTPException(status_code=404, detail="Hábito não encontrado")
